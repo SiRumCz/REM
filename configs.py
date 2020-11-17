aca6d387f924152b0f08adf22d13f8c417264a47
SiRumCZ <sirumcz@gmail.com>
2020-11-17 09:24:30 -0800

    update config

--- configs.py
+++ configs.py
@@ -12,8 +12,7 @@ NPMGRAPH_RELOAD=False
 JSONMODE=False
 FILTER_ENABLE=True
 # rem settings for security advisory dependabot
-REM_HOST='localhost:5000'
-# REM_DEPENDABOT_HTML_OUTDIR='../public_html/dependabot-rem/live'
-# REM_DEPENDABOT_IMG_OUTDIR='../public_html/dependabot-rem/img'
-REM_DEPENDABOT_HTML_OUTDIR='test\\live'
-REM_DEPENDABOT_IMG_OUTDIR='test\\img'
+REM_DEPENDABOT_HTML_URL='http://142.104.68.87/~zkchen/dependabot-rem/live'
+REM_DEPENDABOT_IMG_URL='http://142.104.68.87/~zkchen/dependabot-rem/img'
+REM_DEPENDABOT_HTML_OUTDIR='../public_html/dependabot-rem/live'
+REM_DEPENDABOT_IMG_OUTDIR='../public_html/dependabot-rem/img'
